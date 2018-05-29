becb99a585ab79098043198d78c6f3ee45997968
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-05-29 10:55:41 +0100

    página eventos--incompleta

--- dashboard/js/catalogo.js
+++ dashboard/js/catalogo.js
@@ -1,16 +1,32 @@
+let temparray = []
 window.onload = function () {
 
     //Ir buscar o array de eventos e de categorias atualizado
     rendereventos()
     rendercategorias()
     renderCats()
-
+    
 
     //Ir buscar a tabela
-    let tblEventos = document.getElementById("tblEventos")
+    let tblEventos = document.getElementById("catalog")
+    
+    //Ir buscar o botão
+    let searchBtn = document.getElementById("btnSearch")
+    let selCategorias = document.getElementById("selCategorias")
+    let selFiltros = document.getElementById("selFiltros")
+    let categoria=selCategorias.value
+    renderizararray()
     renderTable()
+    searchBtn.addEventListener("click", function () {
+        switch (selFiltros.value) {
+            case "":
+            tblEventos.innerHTML=""
+                renderizararray(categoria)
+                renderTable()
+                break
+        }
 
-
+    })
 }
 
 
@@ -23,19 +39,18 @@ function renderCats() {
         if (tempCats.indexOf(categorias[i]._id) == -1) {
             // 2. Guardar todos os CatsId não duplicados
             tempCats.push(categorias[i]._id)
-            console.log(tempCats)
         }
     }
-    console.log(tempCats)
+  
     // 3. Criar o HTML (option) para todos as categorias encontradas
     let strHtml = "<option value=''>Lista de categorias</option>"
     for (let i = 0; i < tempCats.length; i++) {
         // Obter o nome da categoria no array users
-        console.log(tempCats[i])
+        
         for (let j = 0; j < categorias.length; j++) {
             if (categorias[j]._id == tempCats[i]) {
                 strHtml += `<option value='${tempCats[i]}'>${categorias[j]._nome}</option>`
-                console.log(categorias[j]._nome)
+                
             }
         }
     }
@@ -44,92 +59,50 @@ function renderCats() {
     selCategorias.innerHTML = strHtml
 }
 
-//Função para renderizar tabela--MAL
+
 
 // Função para renderizar a tabela 
-function renderTable(categoria = "", filtro = "") {
-    //Iterar sobre o array de eventos
-    // 2. Para cada evento vou definir uma Card e compô-la com os dados do objeto
-
-    switch (filtro) {
-        case "": //Renderizar por categoria
-            renderTablecategorias(categoria)
-            break;
-        case "recentes"://Renderizar por recentes
-            break;
-        case "pontuacoes"://Renderizar por pontuaçoes
-            break;
-        case "realizados"://Renderizar já realizados
-            break;
-        case "arealizar"://Renderizar os que ainda vão acontecer
-            break;
-
-
-
-
+function renderizararray(categoria="") {
+    console.log("___"+categoria)
+    for (let i = 0; i < eventos.length; i++) {
+        if (categoria =="" || eventos[i]._categoria.indexOf(categoria) == 1) {
+            temparray.push(eventos[i])
+            console.log(eventos[i]._nome)
+        }
     }
 }
 
-
-
-
-
-function renderTablecategorias(categorias = "") {
-    let strHtmlCard=""
-    //Percorrer o array de eventos
-    for (let i = 0; i < eventos.length; i++) {
-        let campos = eventos[i]._categoria.split(";")
-        //Percorrer o campo
-        for (let j = 0; j < campos.length; j++) {
-
-
-            if ((categorias == "") ||
-                (categorias == campos[j] && campos[j] != "")) {
-
-                // Inicia a linha
-                if (i % 3 == 0) {
-                    strHtmlCard += `<div class="row">`
-                }
-
-                // Cria a card
-                strHtmlCard += `<div class="col-sm-4">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${eventos[i]._imagem}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${eventos[i]._nome}</h5>
-                <p class="card-text">${eventos[i]._sala}</p>
-                <p class="card-text">${eventos[i]._data}</p>
-                <p class="card-text">${eventos[i]._hora}</p>`
-                strHtmlCard += `<a id="${eventos[i]._id}" href="#" class="btn btn-danger remove">Editar</a>`
-
-                strHtmlCard += `<a id="${eventos[i]._id}" href="#" class="btn btn-danger remove">Remover</a>`
-
-                strHtmlCard += `</div>
-        </div>      
-    </div>`
-
-                // Fecha a linha
-                if (i % 3 == 2) {
-                    strHtmlCard += `</div>`
-                    let eventosCatalog = document.getElementById("catalog")
-                    tripsCatalog.innerHTML = strHtmlCard
-
-                    // Obter todos os botões REMOVE
-                    let btnRemove = document.getElementsByClassName("remove")
-                    // Para cada botão, adicionar um listener para escutar pelo evento clique
-                    for (let i = 0; i < btnRemove.length; i++) {
-                        btnRemove[i].addEventListener("click", function () {
-                            // By clicking in a specific game, remove it from the array
-                            let tripId = btnRemove[i].getAttribute("id")
-                            removeTripById(tripId)
-                            renderCatalog(userId)
-                        })
-                    }
-
-                   
-                }
-
-            }
+function renderTable() {
+    let strHtmlCard = ""
+    for (let i = 0; i < temparray.length; i++) {
+        // Inicia a linha
+        if (i % 3 == 0) {
+            strHtmlCard += `<div class="row">`
         }
+
+        // Cria a card
+        strHtmlCard += `<div class="col-sm-6">
+                <div class="card" style="width: 15rem;">
+                    <img class="card-img-top" src="${temparray[i]._imagem}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${temparray[i]._nome}</h5>
+                        <p class="card-text">${temparray[i]._categoria}</p>`
+        
+
+            strHtmlCard += `<a id="${temparray[i]._id}" href="#" class="btn btn-danger remove">REMOVE</a>`
+        
+        strHtmlCard += `</div>
+                </div>      
+            </div>`
+
+        // Fecha a linha
+        if (i % 3 == 2) {
+            strHtmlCard += `</div>`
+        }
+        console.log("O CARD DEVIA ESTAR A DAR"+temparray[i]._nome)
+
     }
+    let tblEventos = document.getElementById("catalog")
+
+    tblEventos.innerHTML=strHtmlCard
 }
