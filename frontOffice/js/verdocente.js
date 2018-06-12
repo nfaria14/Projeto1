fe6ca692caa934ae3c553dba41eec92284ca50fd
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-06-12 14:47:18 +0100

    docentes

---
+++ frontOffice/js/verdocente.js
@@ -0,0 +1,27 @@
+window.onload=function(){
+    login()
+    verificarlogin()
+
+    renderDocentes()
+    let docenteID= localStorage.getItem("docenteID")
+
+    //Referencias html
+    let foto=document.getElementById("foto")
+    let nome=document.getElementById("inputNome")
+    let email=document.getElementById("inputEmail")
+    let uc=document.getElementById("inputUC")
+    let formacao=document.getElementById("inputformacao")
+    let cv=document.getElementById("inputCV")
+
+    for(let i=0;i<docentes.length;i++){
+        if(docentes[i]._id==docenteID){
+            foto.setAttribute("src",docentes[i]._foto)
+            nome.value=docentes[i]._nome
+            email.value=docentes[i]._email
+            uc.value=docentes[i]._uc
+            formacao.value=docentes[i]._formacao
+            cv.value=docentes[i]._cv
+        }
+
+    }
+}
