0d158ff0b482a75d396dde80d66c4f201addebde
JoaoCampos99 <j_henrique_campos@hotmail.com>
2018-06-14 21:47:09 +0100

    myprofile

--- frontOffice/js/myprofile.js
+++ frontOffice/js/myprofile.js
@@ -7,6 +7,22 @@ window.onload=function(){
     let frmEstudante=document.getElementById('frmEstudante')
     frmDocente.style.display='none'
     frmEstudante.style.display='none'
+    //Referências HTML FORM ESTUDANTE
+    let nome=document.getElementById("inputNome1")
+    let email=document.getElementById("inputEmail1")
+    let oldpw=document.getElementById("oldPassword1")
+    let newpw=document.getElementById("newPassword1")
+
+    //Referências HTML FORM DOCENTE
+
+    let nome2=document.getElementById("inputNome")
+    let email2=document.getElementById("inputEmail")
+    let oldpw2=document.getElementById("oldPassword")
+    let newpw2=document.getElementById("newPassword")
+    let uc=document.getElementById("inputUC")
+    let formacao=document.getElementById("inputformacao")
+    let cv=document.getElementById("inputCV")
+
 
     let userID=localStorage.getItem("userID")
     for(let i=0;i<utilizadores.length;i++){
@@ -17,14 +33,26 @@ window.onload=function(){
                 console.log(utilizadores[i]._tipo)
                 console.log("Estudante")
                 frmEstudante.style.display='block'
+                console.log(utilizadores[i]._nome)
+                nome.value=utilizadores[i]._nome
+                email.value=utilizadores[i]._email
             }else if(utilizadores[i]._tipo==1){
                 console.log("Docente")
                 frmDocente.style.display='block'
+                nome2.value=utilizadores[i]._nome
+                email2.value=utilizadores[i]._email
+                for(let j=0;j<docentes.length;j++){
+                    if(docentes[j]._email==email2.value){
+                        uc.value=docentes[j]._uc
+                        cv.value=docentes[j]._cv
+                        formacao.value=docentes[j]._formacao
+
+                    }
+                }
             }
         }
     }
     frmDocente.addEventListener("submit",function(){
-
 
     })
 
